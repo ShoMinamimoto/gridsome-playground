9b08c0c5813d6198b806e21e5afe6462420cad76
Rainer Wagner-Ballner <rainer.wagner-ballner@alphasystems.com>
2021-11-11 12:45:53 +0100

    Fonts make Dicier work

---
+++ src/pages/DicePlayground.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>
+    <p>Dice go here.</p>
+    <p class="dice text-5xl">1 2 3 4 5 6</p>
+    <p class="dice text-5xl">6_ON_D12 ACE_HEARTS</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  name: "DicePlayground"
+}
+</script>
+
+<style scoped>
+  .dice{
+    font-family: Dicier-Round, sans-serif;
+    font-weight: 700;
+    font-feature-settings: "ss03", "cv31";
+  }
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,6 +13,7 @@
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+      <a href="https://speakthesky.itch.io/typeface-dicier" target="_blank" rel="noopener">Dicier Typeface by Speak the Sky</a>
     </p>
 
   </Layout>
